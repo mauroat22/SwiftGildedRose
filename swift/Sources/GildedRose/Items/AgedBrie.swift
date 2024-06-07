ff69b8db5a8904999d2f2e14b522220864306d01
mauro <mauroat22@gmail.com>
2024-06-07 10:04:56 +0200

    AB-1 decorator pattern for item

---
+++ swift/Sources/GildedRose/Items/AgedBrie.swift
@@ -0,0 +1,16 @@
+//
+//  AgedBrie.swift
+//  
+//
+//  Created by Mauro Bianchelli on 2024-06-06.
+//
+
+import Foundation
+
+struct AgedBrie: QualityUpdatable {    
+    var item: Item
+
+    func updateQuality() {
+
+    }
+}
---
+++ swift/Sources/GildedRose/Items/Sulfuras.swift
@@ -0,0 +1,15 @@
+//
+//  Sulfuras.swift
+//  
+//
+//  Created by Mauro Bianchelli on 2024-06-06.
+//
+
+import Foundation
+
+struct Sulfuras: QualityUpdatable {
+    var item: Item
+
+    func updateQuality() {
+    }
+}
