a31cfb6a13f34e98e661083de5510ff645800472
mauro <mauroat22@gmail.com>
2024-06-08 23:00:55 +0200

    AB-2 Sulfuras quality constant

--- swift/Sources/GildedRose/Items/Sulfuras.swift
+++ swift/Sources/GildedRose/Items/Sulfuras.swift
@@ -7,17 +7,25 @@
 
 import Foundation
 
-/// Legendary `Item`, never modifies its `sellIn` nor its `quality`
+/// Legendary `Item`, never modifies its `sellIn` nor its `quality`, which must be always "80"
 ///
 class Sulfuras: ItemProtocol {
+
+    struct Const {
+        static let constantQuality = 80
+    }
 
     init(name: String, sellIn: Int, quality: Int) {
         self.name = name
         self.sellIn = sellIn
-        self.quality = quality
+        self.quality = Sulfuras.Const.constantQuality
     }
     
     var name: String
     var sellIn: Int
-    var quality: Int
+    var quality: Int {
+        didSet {
+            quality = Sulfuras.Const.constantQuality
+        }
+    }
 }
